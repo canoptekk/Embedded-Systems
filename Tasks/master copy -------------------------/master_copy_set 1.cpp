9646273f61e15a20ec1d0d13907205e0cf0190bb
canoptekk <dmfinch@outlook.com>
2022-11-04 06:04:31 +0000

    Update master_copy_set 1.cpp

--- Tasks/master copy -------------------------/master_copy_set 1.cpp
+++ Tasks/master copy -------------------------/master_copy_set 1.cpp
@@ -113,6 +113,140 @@ int main()
         DigitalOut led3(LED3);
         //setting other leds to be used via the variables led2 and led3
 
+
+
+        //1. Printing a string
+        char str1[] = "Welcome to ELEC143";
+        //Displaying a string with printf
+        printf("%s\n", str1);
+        //Viewing a string as an array of characters
+        for (unsigned int n=0; n<sizeof(str1); n++) {
+        printf("Character: %c\tASCII: %u\n", str1[n], str1[n]);
+        }
+        //2. It is the ZERO that defines the end of a string for many functions. 
+        //   Insert a \0 into the middle of the string
+        //   Note the use of strlen
+        char str2[] = "Welcome\0 to ELEC143";
+        printf("%s\n", str2);
+        for (unsigned int n=0; n<strlen(str2); n++) {
+        printf("Character: %c\tASCII: %u\n", str2[n], str2[n]);
+         }
+        //3. Copying strings with strcpy
+        char str3[16];
+        strcpy(str3, "Hello");
+        printf("%s\n", str3);
+        printf("Size: %d\n", sizeof(str3));
+        printf("String length: %d\n", strlen(str3));
+        //strcpy and Constant strings
+        const char str4[] = "The moon is made of cheese";
+        char str5[64];
+        strcpy(str5, str4);
+        printf("%s\n", str5);
+        printf("Size: %d\n", sizeof(str5));
+        printf("String length: %d\n", strlen(str5));   
+        //strcpy(str4, str5);   //Uncomment this
+
+ /*
+    //4. Marshalling binary data with sprintf
+        char str6[8];
+        for (unsigned int n=0; n<3; n++) {
+            unsigned int x = rand();
+            x = x % 65536; //16 bit (2 bytes)
+            sprintf(str6, "I guess %X", x);
+            printf("%s\n", str6);
+        }
+
+
+        //5. Unmarshalling data with sscanf
+        char strList[][8] = {   "1024", 
+                                "32768",
+                                "1234",
+                                "65535"
+                                };
+
+        unsigned int val;
+        for (unsigned int n=0; n<4; n++) {
+            sscanf(strList[n], "%u", &val);
+            printf("Found integer: %u\n", val);
+        }
+
+        //6. Comparing strings with strcmp
+        char inputStr[64];
+        while (true) {
+            printf("What colour?\n");
+            scanf("%s", inputStr);
+
+            if (strcmp(inputStr, "red") == 0) {
+                redLED = !redLED;
+            }
+            else if ( strcmp(inputStr, "green") == 0 ) {
+                greenLED = !greenLED;
+            }
+            else if ( strcmp(inputStr, "blue" ) == 0) {
+                blueLED = !blueLED;
+            }
+            else {
+                printf("Please specify red, green or blue\n");
+            }
+        }
+
+    }
+
+ */
+        //this code is for marshalling and unmarshalling strings, a way of transporting data from one device to another
+        //as well as comparing strings , return to 158 for more details
+
+
+//Writing strings in C++
+
+/*
+    int x = 1234;
+
+    cout << "Hello ";
+    cout << "World:";
+    cout << x << endl;
+
+    string str1 = "Hello World: " + to_string(x);
+    cout << str1 << endl;
+
+    // String input
+    cout << "Type in an integer" << endl;
+    cin >> x;
+    cout << "You typed: " << x << endl;
+
+    // Convert to C string
+    string elec143 = "Welcome to ELEC143"; 
+    const char *str = elec143.c_str();
+    printf("C string is %s\n", str);
+
+    // Convert C string to C++ string
+    char cstr[] = "ELEC143";
+    string cppStr = "Welcome to " + string(cstr);
+    cout << cppStr << endl;
+
+    // Simple string compare
+    string strInput = "      ";
+    while (true) {
+        cout << "What colour?" << endl;
+        cin >> strInput;
+        if (strInput == "red") {
+            redLED = !redLED;
+        }
+        else if (strInput == "green") {
+            greenLED = !greenLED;
+        }
+        else if (strInput == "blue") {
+            blueLED = !blueLED;
+        }
+        else {
+            cout << "Please specify red, green or blue" << endl;
+        }
+    }
+*/
+//same as prior code however for c ++ in particular
+
+
+
         //Dual Digit 7-segment Display
         LatchedLED disp(LatchedLED::SEVEN_SEG);
         //Turn ON the 7-segment display
@@ -285,6 +419,13 @@ int main()
 
         unsigned char ucbyte = 0b10100011;
         ucbyte ^= 0b01000000; //toggle pin 7 using Xor
+
+        //forming an array
+        for (unsigned int m=0; m<100; m++) {
+            unsigned short ldrVal   = ldr.read_u16();
+            samples[m] = ldrVal;
+            wait_us(10000);          // 10ms
+        }
 
 
 
